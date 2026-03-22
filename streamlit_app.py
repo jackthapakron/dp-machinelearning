b2c8a1c5ba8fcaeec0996a0fe44fc952e8433274
jackthapakron <99876651+jackthapakron@users.noreply.github.com>
2026-03-22 20:52:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,12 +33,12 @@ with st.sidebar:
   body_mass_g = st.slider('Body mass (g)', 2700.0,6300.0,4207.0)
   # Create a DataFrame for the input features
 
-  data = ('island':island,
+  data = {'island':island,
           'bill_length_mm':bill_length_mm,
           'bill_depth_mm':bill_depth_mm,
           'flipper_length_mm':flipper_length_mm,
           'body_mass_g':body_mass_g,
-          'gender':gender)
+          'gender':gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 
