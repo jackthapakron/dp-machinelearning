ec08ff2587c5c8d35a3e1991e54a216ab5fa4a1a
jackthapakron <99876651+jackthapakron@users.noreply.github.com>
2026-03-22 19:23:24 +0700

    Fix typo in write function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🎈 Machine learning App')
 st.info('This app build a machine learning model!')
 
 with st.expander('Data'):
-  st.writer('**Raw data**')
+  st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
