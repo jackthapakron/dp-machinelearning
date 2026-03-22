5942a9aebe39df8d3db82f56df3ff1ce18a3224f
jackthapakron <99876651+jackthapakron@users.noreply.github.com>
2026-03-22 20:49:54 +0700

    Fix data structure syntax and update DataFrame creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,15 +33,17 @@ with st.sidebar:
   body_mass_g = st.slider('Body mass (g)', 2700.0,6300.0,4207.0)
   # Create a DataFrame for the input features
 
-  data = ('island',island,
-          'bill_length_mm',bill_length_mm,
-          'bill_depth_mm',bill_depth_mm,
-          'flipper_length_mm',flipper_length_mm,
-          'body_mass_g',body_mass_g,
-          'gender',gender}
+  data = ('island':island,
+          'bill_length_mm':bill_length_mm,
+          'bill_depth_mm':bill_depth_mm,
+          'flipper_length_mm':flipper_length_mm,
+          'body_mass_g':body_mass_g,
+          'gender':gender}
   input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X], axis=0)
 
-input_df
+input_penguins
+  
 
 
 
