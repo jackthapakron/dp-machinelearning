b453785c07e0f9cd9bf2cd06d7ea034369c061a0
jackthapakron <99876651+jackthapakron@users.noreply.github.com>
2026-03-23 21:12:47 +0700

    Refactor target_encode function definition

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,11 @@ with st.sidebar:
   target_mapper = {'Adelie':0,
                    'Chinstrap':1,
                    'Gentoo':2}
-  def target_encode(val):
-    return target_mapper[val]
-    
+def target_encode(val):
+  return target_mapper[val]
+
+y = y_raw.apply(target_encode)
+y
 
 with st.expander('Input features'):
   st.write('**Input penguin**')
