430327d51f0de91b729b2e5e8bfc90d44d656454
jackthapakron <99876651+jackthapakron@users.noreply.github.com>
2026-03-23 21:31:49 +0700

    Fix encoding of features in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,8 @@ with st.sidebar:
   # Encode X
 encode =  ['island','sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
 input_row = df_penguins[:1]
 
   # Encode y
